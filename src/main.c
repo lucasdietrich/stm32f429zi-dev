cc9fc15e96ad7c8902348a90d544d18eb9f77d0d
Lucas Dietrich <ld.adecy@gmail.com>
2022-03-29 12:02:16 +0000

    Minor changes

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	ipc_attach_rx_msgq(&msgq);
 
-	ipc_frame_t frame;
+	static ipc_frame_t frame;
 
 	char addr_str[BT_ADDR_LE_STR_LEN];
 
@@ -30,19 +30,28 @@ int main(void)
 			LOG_INF("Received BLE Xiaomi records count: %u, frame_time: %u",
 				dataframe->count, dataframe->frame_time);
 
+			uint32_t frame_ref_time = dataframe->frame_time;
+
 			// Show all records
 			for (uint8_t i = 0; i < dataframe->count; i++) {
-				bt_addr_le_to_str(&dataframe->records[i].addr, addr_str,
+				xiaomi_record_t *const rec = &dataframe->records[i];
+
+				bt_addr_le_to_str(&rec->addr, 
+						  addr_str,
 						  sizeof(addr_str));
 
+				int32_t record_rel_time = rec->uptime - frame_ref_time;
+
 				 // Show BLE address, temperature, humidity, battery
-				LOG_INF("\tBLE Xiaomi record %u: addr: %s, temp: %d.%d °C, hum: %u %%, bat: %u mV",
+				LOG_INF("\tBLE Xiaomi record %u [%d s]: addr: %s, " \
+					"temp: %d.%d °C, hum: %u %%, bat: %u mV",
 					i,
+					record_rel_time,
 					log_strdup(addr_str),
-					dataframe->records[i].data.temperature / 100,
-					dataframe->records[i].data.temperature % 100,
-					dataframe->records[i].data.humidity,
-					dataframe->records[i].data.battery);
+					rec->measurements.temperature / 100,
+					rec->measurements.temperature % 100,
+					rec->measurements.humidity,
+					rec->measurements.battery);
 			}
 		}
 	}
