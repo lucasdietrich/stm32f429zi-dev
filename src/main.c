0a496242fd1f6fa740f908ed4101bede6a736e43
Lucas Dietrich <ld.adecy@gmail.com>
2022-04-26 18:02:14 +0000

    (WIP) Implement TX

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <zephyr.h>
 
+#include <string.h>
+
+#include "uart_ipc/ipc.h"
+
+// #include <uart_ipc/ipc.h>
+
+#define MESSAGE "IPC"
+
 int main(void)
 {
-	return 0;
+	ipc_data_t data;
+
+	memcpy(data.buf, MESSAGE, sizeof(MESSAGE));
+	data.size = sizeof(MESSAGE);
+
+	k_sleep(K_SECONDS(2));
+
+	for (;;) {
+		ipc_send_data(&data);
+
+		k_sleep(K_SECONDS(10));
+	}
 }
